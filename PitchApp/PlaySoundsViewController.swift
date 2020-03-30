cb28657e7b743bd5a119aa064631bb10ea706762
Artem Osipov <ao@enlighted.ru>
2020-03-30 21:30:33 +0300

    initial commit (actually last, I hope)

---
+++ PitchApp/PlaySoundsViewController.swift
@@ -0,0 +1,68 @@
+//
+//  PlaySoundsViewController.swift
+//  PitchApp
+//
+//  Created by Artem Osipov on 30/03/2020.
+//  Copyright © 2020 Artem Osipov. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class PlaySoundsViewController: UIViewController {
+    
+    var recordedAudioURL: URL!
+
+    @IBOutlet weak var slowButton: UIButton!
+    @IBOutlet weak var fastButton: UIButton!
+    @IBOutlet weak var highPitchButton: UIButton!
+    @IBOutlet weak var lowPitchButton: UIButton!
+    @IBOutlet weak var echoButton: UIButton!
+    @IBOutlet weak var reverbationButton: UIButton!
+    @IBOutlet weak var stopButton: UIButton!
+    
+    var audioFile:AVAudioFile!
+    var audioEngine:AVAudioEngine!
+    var audioPlayerNode: AVAudioPlayerNode!
+    var stopTimer: Timer!
+    
+    enum ButtonType: Int {
+        case slow = 0, fast, chipmunk, vader, echo, reverb
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setupAudio()
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        configureUI(.notPlaying)
+    }
+    
+    @IBAction func playSoundForButton(_ sender: UIButton) {
+        switch(ButtonType(rawValue: sender.tag)!) {
+        case .slow:
+            playSound(rate: 0.5)
+        case .fast:
+            playSound(rate: 1.5)
+        case .chipmunk:
+            playSound(pitch: 1000)
+        case .vader:
+            playSound(pitch: -1000)
+        case .echo:
+            playSound(echo: true)
+        case .reverb:
+            playSound(reverb: true)
+        }
+
+        configureUI(.playing)
+    }
+    
+    @IBAction func stopButtonTapped(_ sender: Any) {
+        stopAudio()
+    }
+    
+
+
+}
---
+++ PitchApp/RecordSoundsViewController.swift
@@ -0,0 +1,86 @@
+//
+//  RecordSoundsViewController.swift
+//  PitchApp
+//
+//  Created by Artem Osipov on 29/03/2020.
+//  Copyright © 2020 Artem Osipov. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class RecordSoundsViewController: UIViewController, AVAudioRecorderDelegate {
+    
+    var audioRecorder: AVAudioRecorder!
+
+    @IBOutlet weak var recordBtn: UIButton!
+    
+    @IBOutlet weak var recordingLabel: UILabel!
+    @IBOutlet weak var stopRecordingBtn: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        configureUI(false)
+    }
+
+    @IBAction func recordTapped(_ sender: Any) {
+        configureUI(true)
+        
+        let dirPath = NSSearchPathForDirectoriesInDomains(.documentDirectory,.userDomainMask, true)[0] as String
+        let recordingName = "recordedVoice.wav"
+        let pathArray = [dirPath, recordingName]
+        let filePath = URL(string: pathArray.joined(separator: "/"))
+
+        let session = AVAudioSession.sharedInstance()
+        try! session.setCategory(AVAudioSession.Category.playAndRecord, mode: AVAudioSession.Mode.default, options: AVAudioSession.CategoryOptions.defaultToSpeaker)
+
+        try! audioRecorder = AVAudioRecorder(url: filePath!, settings: [:])
+        audioRecorder.delegate = self
+        audioRecorder.isMeteringEnabled = true
+        audioRecorder.prepareToRecord()
+        audioRecorder.record()
+    }
+    
+    @IBAction func stopRecordTapped(_ sender: Any) {
+
+        
+        configureUI(false)
+        
+        audioRecorder.stop()
+        let audioSession = AVAudioSession.sharedInstance()
+        try! audioSession.setActive(false)
+    }
+    
+    
+    func configureUI(_ isRecording: Bool) {
+        recordBtn.isEnabled = !isRecording
+        stopRecordingBtn.isEnabled = isRecording
+        
+        if (!isRecording) {
+            recordingLabel.text = "Tap to record"
+        } else {
+            recordingLabel.text = "Recording in Progress"
+        }
+        
+    }
+    
+    func audioRecorderDidFinishRecording(_ recorder: AVAudioRecorder, successfully flag: Bool) {
+        if (flag) {
+            performSegue(withIdentifier: "stopRecording", sender: recorder.url)
+        }
+        
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if (segue.identifier == "stopRecording") {
+            let playSoundsVC = segue.destination as! PlaySoundsViewController
+            playSoundsVC.recordedAudioURL = sender as! URL
+        }
+    }
+}
+
